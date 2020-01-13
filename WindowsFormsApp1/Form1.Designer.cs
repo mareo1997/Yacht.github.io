2672808984bb453d4c6457a510145d65f590d594
mareo1997 <mareo1997@gmail.com>
2020-01-13 15:42:07 -0500

    First draft

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,13 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Type = new System.Windows.Forms.Label();
+            this.Destination = new System.Windows.Forms.Label();
+            this.Time = new System.Windows.Forms.Label();
+            this.Submit = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // Type
+            // 
+            this.Type.AutoSize = true;
+            this.Type.Location = new System.Drawing.Point(12, 87);
+            this.Type.Name = "Type";
+            this.Type.Size = new System.Drawing.Size(31, 13);
+            this.Type.TabIndex = 1;
+            this.Type.Text = "Type";
+            // 
+            // Destination
+            // 
+            this.Destination.AutoSize = true;
+            this.Destination.Location = new System.Drawing.Point(269, 87);
+            this.Destination.Name = "Destination";
+            this.Destination.Size = new System.Drawing.Size(60, 13);
+            this.Destination.TabIndex = 2;
+            this.Destination.Text = "Destination";
+            // 
+            // Time
+            // 
+            this.Time.AutoSize = true;
+            this.Time.Location = new System.Drawing.Point(476, 87);
+            this.Time.Name = "Time";
+            this.Time.Size = new System.Drawing.Size(30, 13);
+            this.Time.TabIndex = 3;
+            this.Time.Text = "Number of nights";
+            // 
+            // Submit
+            // 
+            this.Submit.Location = new System.Drawing.Point(272, 285);
+            this.Submit.Name = "Submit";
+            this.Submit.Size = new System.Drawing.Size(75, 23);
+            this.Submit.TabIndex = 5;
+            this.Submit.Text = "Submit";
+            this.Submit.UseVisualStyleBackColor = true;
+            this.Submit.Click += new System.EventHandler(this.Submit_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Sailing",
+            "Powered",
+            "Crewed"});
+            this.comboBox1.Location = new System.Drawing.Point(15, 131);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Americas",
+            "Caribbean",
+            "Mediterranean"});
+            this.comboBox2.Location = new System.Drawing.Point(272, 130);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 8;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            1,
+            7,
+            14});
+            this.comboBox3.Location = new System.Drawing.Point(479, 130);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 9;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Submit);
+            this.Controls.Add(this.Time);
+            this.Controls.Add(this.Destination);
+            this.Controls.Add(this.Type);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Label Type;
+        private System.Windows.Forms.Label Destination;
+        private System.Windows.Forms.Label Time;
+        private System.Windows.Forms.Button Submit;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,57 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        int type, dest, time, result;
         public Form1()
         {
             InitializeComponent();
+           // if (!comboBox2.Text.Equals(null))
+           // {
+                Submit.Enabled = false;
+            //}
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem.Equals("Sailing")) {
+                type = 1000;
+            }
+            else if (comboBox1.SelectedItem.Equals("Powered"))
+            {
+                type = 3000;
+            }
+            else if (comboBox1.SelectedItem.Equals("Crewed"))
+            {
+                type = 5000;
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Submit.Enabled = true;
+            dest = 10;
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox3.SelectedItem.Equals(1))
+            {
+                time = 1;
+            }
+            else if (comboBox3.SelectedItem.Equals(7))
+            {
+                time = 7;
+            }
+            else if (comboBox3.SelectedItem.Equals(14))
+            {
+                time = 14;
+            }
+        }
+
+        private void Submit_Click(object sender, EventArgs e)
+        {
+            result = type + dest * time;
+            Submit.Text = "$" + result.ToString();
         }
     }
 }
