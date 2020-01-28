c819e56d65630c306def5d316a904a7b2c87a4a7
mareo1997 <50336724+mareo1997@users.noreply.github.com>
2020-01-28 18:38:43 -0500

    Add files via upload

--- HomePage.js
+++ HomePage.js
@@ -11,6 +11,22 @@ $('#type').html(data.type)
 $('#dest').html(data.dest)
 $('#time').html(data.time)
 
+function myHOME(){
+	window.open("Home.html", "_self")
+}
+
+function myORDER(){
+	window.open("Options.html", "_self")
+}
+
+function mySingle(){
+	window.open("Single.html", "_self")
+}
+
+function myMulti(){
+	window.open("Single.html", "_self")
+}
+
 function validateForm(){
 	window.open("Login.html")
 }
@@ -19,12 +35,20 @@ function myFAQ(){
 	window.open("FAQ.html", "_self")
 }
 
-function myOPTIONS(){
-	window.open("Options.html", "_self")
-}
-
-function myHOME(){
-	window.open("Home.html", "_self")
+function myQUOTE(){
+	var a = document.getElementById("types");
+	var c = document.getElementById("dests");
+	var e = document.getElementById("times");
+	var g = document.getElementById("budget");
+	
+	var b = x.selectedIndex;
+	var d = x.selectedIndex;
+	var f = x.selectedIndex;
+	var h = x.selectedIndex;
+	
+	var i;
+	
+	document.getElementById("demo").innerHTML = a.options[b].text;
 }
 
 console.log(data)
