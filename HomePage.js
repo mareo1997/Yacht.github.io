f3956868473bbf04a227c173e2a0fb824f2e0991
mareo1997 <mareo1997@gmail.com>
2020-02-03 15:34:43 -0500

    Added information about the sail yachts available including things like the max capacity, number of toilets, showers and a starting price. This approach will allowed for the options and single webpage to be removed.

--- HomePage.js
+++ HomePage.js
@@ -15,14 +15,6 @@ function myHOME(){
 	window.open("Home.html", "_self")
 }
 
-function myORDER(){
-	window.open("Options.html", "_self")
-}
-
-function mySingle(){
-	window.open("Single.html", "_self")
-}
-
 function validateForm(){
 	window.open("index/index.html", "_self")
 }
@@ -92,45 +84,6 @@ function myBackgroundimg(){
 	document.body.style.backgroundImage = "url('https://www.fraseryachts.com/uploads/image/yachts/queen-anne/Gentech-yachts_yacht_" +
 			"for_sale_Queen-anne_12467.jpg')";
 }
-
-function myQUOTE(){
-	var a = document.getElementById("types");
-	var e = document.getElementById("times");
-	var g = document.getElementById("budget");
-	
-	var b = x.selectedIndex;
-	var f = x.selectedIndex;
-	var h = x.selectedIndex;
-	
-}
-
-function a(){		
-	var x = document.getElementById("destination").selectedIndex;
-	var y = document.getElementById("destination").options;
-	document.getElementById("destination").selectedIndex = y[x].index;
-	var d = c.selectedIndex;	
-	document.getElementById("destination").selectedIndex = d
-}
-
-function myFilter() {
-	  document.getElementById("destination").classList.toggle("show");
-}
-
-	function filterFunction() {
-	  var input, filter, ul, li, a, i;
-	  input = document.getElementById("myInput");
-	  filter = input.value.toUpperCase();
-	  div = document.getElementById("myDropdown");
-	  a = div.getElementsByTagName("a");
-	  for (i = 0; i < a.length; i++) {
-	    txtValue = a[i].textContent || a[i].innerText;
-	    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-	      a[i].style.display = "";
-	    } else {
-	      a[i].style.display = "none";
-	    }
-	  }
-	}
 
 function more1(){
 	var w = document.getElementById("americas");
